ab099170b2ce8eebb98dc331c16967be456ec62e
clford <connorford23@gmail.com>
2020-12-24 12:30:46 -0600

    Added mapPreview, updated matchupPreview

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, flash, redirect
-from models import CoDStats
+from models import CoDStats, Team
 
 app = Flask(__name__)
+
+stats = CoDStats()
 
 @app.route("/")
 def index():
@@ -11,20 +13,72 @@ def index():
 def statsEntry():
     return render_template("statsEntry.html")
 
-@app.route("/viewStats", methods=["GET", "POST"])
-def viewStats():
+@app.route("/viewMatches", methods=["GET", "POST"])
+def viewMatches():
     if request.method == "POST":
         team = request.form.get("team")
         opponent = request.form.get("opponent")
         if team != "All" and opponent == "All":
-            results = CoDStats().getTeamMatches(team)
+            results = stats.getTeamMatches(team)
         elif team != "All" and opponent != "All":
-            results = CoDStats().getTeamOppMatches(team, opponent)
+            results = stats.getTeamOppMatches(team, opponent)
         else:
-            results = CoDStats().getAllMatches()
+            results = stats.getAllMatches()
     else:
-        results = CoDStats().getAllMatches()
-    return render_template("viewStats.html", tables=[results.to_html(classes="data")])
+        results = stats.getAllMatches()
+    return render_template("viewMatches.html", teams=stats.teams, tables=[results.to_html(classes="data")])
+
+@app.route("/viewMaps", methods=["GET", "POST"])
+def viewMaps():
+    if request.method == "POST":
+        team = request.form.get("team")
+        mode = request.form.get("mode")
+        if team != "All" and mode != "All": # team and mode
+            results = stats.getTeamModeMaps(team, mode)
+        elif team != "All" and mode == "All": # team
+            results = CoDStats.getTeamMaps(team)
+        elif team == "All" and mode != "All": # mode
+            results = stats.getModeMaps(mode)
+        else:
+            results = stats.getTeamMaps(team)
+    else:
+        results = stats.getAllMaps()
+    return render_template("viewMaps.html", teams=stats.teams, tables=[results.to_html(classes="data")])
+
+@app.route("/viewPlayers", methods=["GET", "POST"])
+def viewPlayers():
+    kills = 0
+    deaths = 0
+    kd = 0
+    player = ""
+    if request.method == "POST" or request.method == "GET":
+        player = request.form.get("player")
+        kills, deaths, kd = stats.getPlayerKD(player)
+    return render_template("viewPlayers.html", players=stats.playerList, kills=kills, player=player, deaths=deaths, kd=kd)
+
+@app.route("/matchupPreview", methods=["GET", "POST"])
+def matchupPreview():
+    team1 = Team()
+    team2 = Team()
+    if request.method == "POST":
+        team1 = Team(request.form.get("team1"))
+        team2 = Team(request.form.get("team2"))
+    return render_template("matchupPreview.html", team1=team1, team2=team2, stats=stats)
+
+@app.route("/mapPreview", methods=["GET", "POST"])
+def mapPreview():
+    team1 = Team()
+    team2 = Team()
+    maps = []
+    if request.method == "POST":
+        team1 = Team(request.form.get('team1'))
+        team2 = Team(request.form.get('team2'))
+        maps = [request.form.get('map1'),
+                request.form.get('map2'),
+                request.form.get('map3'),
+                request.form.get('map4'),
+                request.form.get('map5')]
+    return render_template("mapPreview.html", team1=team1, team2=team2, stats=stats, maps=maps)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -6,7 +6,22 @@ class CoDStats:
     def __init__(self):
         self.matches = pd.read_csv("data/matches.csv")
         self.maps = pd.read_csv("data/maps.csv")
+        self.players = pd.read_csv("data/players.csv")
+        self.modes = self.maps['Game Mode'].unique()
+        self.modeMaps = [0, 0, 0]
+        self.modeMaps[0] = self.maps[self.maps['Game Mode'] == self.modes[0]]['Map'].unique()
+        self.modeMaps[0].sort()
+        self.modeMaps[1] = self.maps[self.maps['Game Mode'] == self.modes[1]]['Map'].unique()
+        self.modeMaps[1].sort()
+        self.modeMaps[2] = self.maps[self.maps['Game Mode'] == self.modes[2]]['Map'].unique()
+        self.modeMaps[2].sort()
+        self.playerList = self.players['Player'].unique()
+        self.playerList.sort()
+        self.teams = pd.unique(self.matches[['Team 1', 'Team 2']].values.ravel())
+        self.teams.sort()
+        self.events = self.matches['Event'].unique()
 
+    ############### MATCH FUNCTIONS ###############
     def getTeamMatches(self, team):
         return self.matches[(self.matches['Team 1'] == team) | (self.matches['Team 2'] == team)]
 
@@ -15,8 +30,18 @@ class CoDStats:
                             ((self.matches['Team 2'] == team) & (self.matches['Team 1'] == opponent))]
 
     def getOverallRecord(self, team):
-        wins = 1
-        losses = 2
+        wins = self.matches['Winner'].value_counts()[team]
+        losses = self.matches['Team 1'].value_counts()[team] + self.matches['Team 2'].value_counts()[team] - wins
+        return wins, losses
+
+    def getMapRecord(self, team):
+        wins = self.maps['Map Winner'].value_counts()[team]
+        losses = self.maps['Team 1'].value_counts()[team] + self.maps['Team 2'].value_counts()[team] - wins
+        return wins, losses
+
+    def getModeRecord(self, team, mode):
+        wins = self.maps[self.maps['Game Mode'] == mode]['Map Winner'].value_counts()[team]
+        losses = self.maps[self.maps['Game Mode'] == mode]['Team 1'].value_counts()[team] + self.maps[self.maps['Game Mode'] == mode]['Team 2'].value_counts()[team] - wins
         return wins, losses
 
     def getAllMatches(self):
@@ -24,3 +49,61 @@ class CoDStats:
 
     def getTeamWins(self, team):
         return self.matches[self.matches['Winner'] == team]
+
+    ############### MAP FUNCTIONS ###############
+    def getAllMaps(self):
+        return self.maps
+
+    def getTeamMaps(self, team):
+        return self.maps[(self.maps['Team 1'] == team) | (self.maps['Team 2'] == team)]
+
+    def getTeamModeMaps(self, team, mode):
+        return self.maps[((self.maps['Team 1'] == team) & (self.maps['Game Mode'] == mode)) |
+                         ((self.maps['Team 2'] == team) & (self.maps['Game Mode'] == mode))]
+
+    def getModeMapRecord(self, team, mode, map):
+        # Returns win-loss record for given team, mode, and map
+
+    ############### PLAYER FUNCTIONS ###############
+    def getPlayerKD(self, player):
+        stats = self.players[self.players['Player'] == player]
+        kills = stats['Kills'].sum()
+        deaths = stats['Deaths'].sum()
+        kd = round(kills / deaths, 2)
+        return kills, deaths, kd
+
+    def getAllPlayers(self):
+        results = self.players['Player'].unique()
+        results.sort()
+        return results
+
+    ############### MATCHUP PREVIEW ###############
+    def getMatchupPreview(self, team1, team2):
+        # TODO
+        return
+
+    ############### MAP PREVIEW ###############
+    def getMapPreview(self, team1, team2, maps):
+        # TODO
+        return
+
+
+class Team:
+
+    def __init__(self, name="Team"):
+        stats = CoDStats()
+        self.name = name
+        self.modeWins = [0, 0, 0]
+        self.modeLosses = [0, 0, 0]
+        if self.name != "Team":
+            self.wins, self.losses = stats.getOverallRecord(name)
+            self.mapWins, self.mapLosses = stats.getMapRecord(name)
+            for k in range(3):
+                self.modeWins[k], self.modeLosses[k] = stats.getModeRecord(name, stats.modes[k])
+        else:
+            self.wins = 0
+            self.losses = 0
+            self.mapWins = 0
+            self.mapLosses = 0
+
+# Same kind of class except for Player?
